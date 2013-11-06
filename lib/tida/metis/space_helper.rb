8a437b6a1a507338660ccdc3b8612c11c56fcde0
babaru <babaru.trinit@gmail.com>
2013-11-07 07:12:18 +0800

    fix current_space bug

--- lib/tida/metis/space_helper.rb
+++ lib/tida/metis/space_helper.rb
@@ -2,7 +2,7 @@ module Tida
   module Metis
     module SpaceHelper
       def current_space
-        if current_user && !current_user.is_sys_admin?
+        if current_user && current_user.spaces.count > 0 && !current_user.is_sys_admin?
           session[:space_id] = current_user.spaces.first.id unless session[:space_id]
           return Space.find(session[:space_id]) if session[:space_id]
         end
