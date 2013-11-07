a7824ccf0763b99db45c7c80b0b8f8d81b1c036a
babaru <babaru.trinit@gmail.com>
2013-11-07 14:14:37 +0800

    fix executing projects bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
 
   def index
     if @space
-      @projects_grid = initialize_grid(Project.joins(:client).where('clients.space_id=?', @space.id).order('client_id'))
+      @projects_grid = initialize_grid(Project.joins(:client).where('clients.space_id=?', @space.id).where('started_at <= ? AND ended_at >= ?', Time.now.strftime('%Y-%m-%d'), Time.now.strftime('%Y-%m-%d')).order('client_id'))
     end
   end
 end
